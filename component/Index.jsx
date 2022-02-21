b4e52a2f5b32f15a612b4bb2e87b176e05e1bf88
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2022-02-21 11:57:58 +0530

    Update Index.jsx

--- component/Index.jsx
+++ component/Index.jsx
@@ -1,30 +1,46 @@
-import React,{useState} from 'react'
-import Image from 'next/image'
+import React,{useEffect, useState} from 'react';
+import Image from 'next/image';
 
 const Index = () => {
     const [apiData,setapiData]=useState([]);
-    fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json())
-    .then(json=>setapiData(json))
-
+    const [filterData,setfilterData]=useState("");
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json=>setapiData(json))
+    },[])
+   
+const filterHandler=(event)=>{
+    setfilterData(event.target.value);
+}
   return (
     <div>
         
-<div className="container">
+        <div className="container">
 
-<h1>Shopping Cart</h1>
+        <center><h1>Shopping Cart</h1></center> <br/>
+        <label><b>Filter By Category/Name: </b></label>
+        <input type="text" onChange={filterHandler}/> <br/> <br/>
 
-<div className="cart">
+        <div className="cart">
 
-    <div className="products">
-{
-    apiData.map((curelem)=>{
-        return( 
-        <div className="product" key={curelem.id}>
+            <div className="products">
+            {
+                apiData.filter((curelems)=>{
+                    if(filterData === ""){
+                    return curelems;
+                    } else if(curelems.title.toLowerCase().includes(filterData.toLowerCase())){
+                    return curelems;
+                    } else if(curelems.category.toLowerCase().includes(filterData.toLowerCase())){ 
+                    return curelems; 
+                    } 
+            }).map((curelem)=>{
+            return( 
+                <div className="product" key={curelem.id}>
 
-            <Image src={curelem.image} alt="no " width="200px" height="200px"/>
+                <Image src={curelem.image} alt="no " width="200px" height="200px"/>
 
-            <div className="product-info">
+                <div className="product-info">
 
 
                 <h3 className="product-name">{curelem.title}</h3>
@@ -33,8 +49,6 @@ const Index = () => {
 
                 <h4 className="product-offer">Category: {curelem.category}</h4>
 
-                
-
                 <p className="product-detail">
 
                     <i className="fa fa-detail" aria-hidden="true"></i>
@@ -42,25 +56,17 @@ const Index = () => {
                     <span className="detail">More Details</span>
                 </p>
 
-            </div>
+                </div>
+
+                </div>
+            )
+
+            })
+            }
+            </div> 
+        </div>
 
         </div>
-        )
-
-    })
-}
-
-        
-      
-    
-
-    </div>
-
-    
-
-</div>
-
-</div>
 
     </div>
     
